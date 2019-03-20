311951f23fafa3709698dd29cb0faf65d5b4a425
Michael Frangione <mfrangione@gmail.com>
2019-03-20 17:22:27 -0400

    Updated env variables

--- src/components/Searchbox.js
+++ src/components/Searchbox.js
@@ -1,4 +1,4 @@
-import React, {Fragment} from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import GraphQLActions from '../graphQl/GraphQLActions';
 import { onSearch, clearSearchResults, updateSearchTerm, onSearchError, updateLoadingState } from "../actions/index";
